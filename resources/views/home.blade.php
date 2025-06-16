07f72017cf921950fe6c143aa612fce700e84eb4
mehrad <mmfirst@protonmail.com>
2025-06-16 20:33:24 +0330

    feat: replaced welcome with basic routes list

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<h1>use routes :</h1>
+<ul>
+    <li> GET / </li>
+    <li> GET api </li>
+    <li> GET api/blog </li>
+    <li> POST api/blog </li>
+    <li> GET api/blog/{blog} </li>
+    <li> PUT|PATCH api/blog/{blog} </li>
+    <li> POST api/comment </li>
+    <li> DELETE api/blog/{blog} </li>
+    <li> PUT|PATCH api/comment/{comment} </li>
+    <li> DELETE api/comment/{comment} </li>
+    <li> POST api/login </li>
+    <li> POST api/register </li>
+    <li> GET api/user </li>
+    <li> GET blog/{blog}/pdf </li>
+</ul>
